eb0cfe1a61eadc32adbd1d7dbf22174bfd49222d
Yurii Malchevskyi <yuri.malczewski@gmail.com>
2020-04-29 00:31:44 +0300

    added travis.yml and build tasks

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -33,6 +33,12 @@ module.exports = function (config) {
 		autoWatch: true,
 		browsers: ['Chrome'],
 		singleRun: false,
-		restartOnFileChange: true
+		restartOnFileChange: true,
+		customLaunchers: {
+			ChromeHeadlessNoSandbox: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+		}
 	});
 };
