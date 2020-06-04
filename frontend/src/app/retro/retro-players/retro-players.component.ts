98ec44283fbb31141d797b2eeedd6c1c55a625a1
Yurii Malchevskyi <yuri.malczewski@gmail.com>
2020-06-04 22:57:18 +0300

    draft players component

--- frontend/src/app/retro/retro-players/retro-players.component.ts
+++ frontend/src/app/retro/retro-players/retro-players.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { RetroState } from '@app/retro/retro-state.class';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { RetroState, RetroPlayer } from '@app/retro/retro-state.class';
+import { UserStateService } from '@app/common/user-state.service';
 
 @Component({
 	selector: 'retro-players',
 	templateUrl: './retro-players.component.html',
 	styleUrls: ['./retro-players.component.scss']
 })
-export class RetroPlayersComponent implements OnInit {
+export class RetroPlayersComponent implements OnInit, OnChanges {
 	@Input() state: RetroState;
 
-	constructor() { }
+	highlightedPlayers: {[uid: string]: any} = {};
+
+	constructor(
+		private userState: UserStateService) { }
 
 	ngOnInit(): void {
 	}
+	
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.state.currentValue !== changes.state.previousValue) {
+			if (this.state.lastPlayerUpdate)
+				this.highlightPlayer(this.state.lastPlayerUpdate);
+		}
+	}
+
+	private highlightPlayer(uid: string): void {
+		if (this.highlightedPlayers[uid])
+			clearTimeout(this.highlightedPlayers[uid]);
+		this.highlightedPlayers[uid] = setTimeout(() => {
+				delete this.highlightedPlayers[uid];
+		}, 500);
+	}
+
+	isMyUser(user: RetroPlayer): boolean {
+		return this.userState.getUid() === user.uid;
+	}
+
 
 }
